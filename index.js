9694d1f08c0cd2c7296982a35441e85f164ad435
rochafialvin <rochafi.dev@gmail.com>
2019-07-15 16:08:42 +0700

    First commit

---
+++ index.js
@@ -0,0 +1,47 @@
+/*
+Ini komentar baris satu
+baris dua
+baris tiga
+*/
+
+// Perintah untuk print sesuatu di terminal
+// console.log('Hello world !')
+
+// Print lokasi folder kita saat ini
+// console.log(__dirname)
+
+// Print lokasi file kita saat ini
+// console.log(__filename)
+
+// CTRL + J, munculkan terminal
+// CTRL + /, Membuat / menghilangkan komentar
+
+// VARIABLE
+// Tempat menyimpan satu data
+
+// Membuat variable 'nama', di isi data 'Andi'
+// var nama = 'Andi'
+// Ubah nilai pada variable 'nama' menjadi 'Puck'
+// nama = 'Puck'
+// console.log(nama) 
+
+// var usia
+// usia = 22
+// usia = 45
+// console.log(usia)
+
+// let tidur = true // boolean: true dan false
+// console.log(tidur)
+
+
+// VAR VS LET
+
+// Tidak masalah
+// var hari = 'Senin'
+// var hari = 'Selasa'
+// console.log(hari)
+
+// Error, karena mendeklarasikan variable yang sudah ada
+let buah = 'Apel'
+let buah = 'Semangka'
+console.log(buah)
