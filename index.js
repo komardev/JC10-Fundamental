9fe4e5ae8b31e01c0c610100fe7eded0c77748af
rochafialvin <rochafi.dev@gmail.com>
2019-07-16 14:53:36 +0700

    String and number

--- index.js
+++ index.js
@@ -37,11 +37,31 @@ baris tiga
 // VAR VS LET
 
 // Tidak masalah
-// var hari = 'Senin'
+var hari = 'Senin'
 // var hari = 'Selasa'
 // console.log(hari)
 
 // Error, karena mendeklarasikan variable yang sudah ada
-let buah = 'Apel'
-let buah = 'Semangka'
-console.log(buah)
+// let buah = 'Apel'
+// let buah = 'Semangka'
+// console.log(buah)
+
+
+// Strings & Numbers
+// Strings --> Teks / kumpulan karakter di apit tanda kutip
+// Numbers : 1. Integer (Bilangan bulat: 0, 1, 23)
+//           2. Float (Bilangan decimal: 0.45, 0.89)
+
+// var tempat
+// var _underscore
+// var $dollarsign
+
+var nama = 'Zondo' // string
+var tanggal = 22 // number
+var single = true
+var profesi
+
+var hasil = typeof(profesi)
+console.log(hasil)
+
+console.log(typeof(nama))
