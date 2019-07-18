65d71505335b68cdee66b7d4cf4d2cb47fe70bcd
rochafialvin <rochafi.dev@gmail.com>
2019-07-18 14:31:02 +0700

    Date

--- solveit.js
+++ solveit.js
@@ -28,28 +28,72 @@
 
 //  SOLVE IT 4
 
-var jumlahHari = hariAwal = 485 
+// var jumlahHari = hariAwal = 485 
 
 // Cari berapa tahun
-var tahun = Math.floor(jumlahHari / 360)
+// var tahun = Math.floor(jumlahHari / 360)
 // Cari sisa hari setelah tahun
-jumlahHari = jumlahHari % 360
+// jumlahHari = jumlahHari % 360
 
-var bulan = Math.floor(jumlahHari / 30)
-jumlahHari = jumlahHari % 30
+// var bulan = Math.floor(jumlahHari / 30)
+// jumlahHari = jumlahHari % 30
 
+// var minggu = Math.floor(jumlahHari / 7)
+// jumlahHari = jumlahHari % 7
 
-var minggu = Math.floor(jumlahHari / 7)
-jumlahHari = jumlahHari % 7
+// \n untuk menggeser kalimat berikutnya di baris baru
+// console.log(
+    // hariAwal + ' hari : \n' +
+    // tahun + ' tahun\n' +
+    // bulan + ' bulan\n'  +
+    // minggu + ' minggu\n' +
+    // jumlahHari + ' hari'
+    
+// )
+
+// SOLVE IT 5
+// A + B = 49
+// var totalUmur = 49
+// Rasio = 0.4 -> 4 / 10 -> 2 / 5 -> 2 : 5
+// Total Rasio = 2 + 5 = 7
+
+// atas dan bawah dibagi dengan angka yang sama 
+// 30 / 15 -> 6 / 3 -> 2 / 1
+
+// var rasioAndi = 2
+// var rasioBudi = 5
+// var totalRasio = 7
+
+// var Andi = totalUmur * (rasioAndi / totalRasio)
+// var Budi = totalUmur * (rasioBudi / totalRasio)
+
+// var AndiNext  = Andi + 2
+// var BudiNext  = Budi + 2
+
+// console.log('Andi :' + (Andi + 2));
+// console.log('Budi: ' + (Budi + 2));
+// console.log('Andi :' + AndiNext);
+// console.log('Budi: ' + BudiNext);
+
+//  SOLVE IT 6
+
+var today = new Date()
+var yesterday = new Date()
+var tomorrow = new Date()
+
+tomorrow.setDate(today.getDate() + 1)
+yesterday.setDate(today.getDate() - 1)
+
+console.log(today);
+console.log(yesterday);
+console.log(tomorrow);
 
 console.log(
-    hariAwal + ' hari : \n' +
-    tahun + ' tahun\n' +
-    bulan + ' bulan\n'  +
-    minggu + ' minggu\n' +
-    jumlahHari + ' hari'
-    
-)
+    'Hari ini: ' + today.getDate() + '-' + (today.getMonth() + 1) + '-' + today.getFullYear()
+    + '\nHari ini: ' + yesterday.getDate() + '-' + (yesterday.getMonth() + 1) + '-' + yesterday.getFullYear()
+    + '\nHari ini: ' + tomorrow.getDate() + '-' + (tomorrow.getMonth() + 1) + '-' + tomorrow.getFullYear()
+);
+
 
 
 
