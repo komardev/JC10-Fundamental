a2d3792e2acac725ac0efd46b19f6ee3183b67c4
rochafialvin <rochafi.dev@gmail.com>
2019-07-19 16:38:19 +0700

    WOTACLUB

---
+++ index2.js
@@ -0,0 +1,40 @@
+// LOOP
+// Melakukan suatu proses selama kondisi tertentu terpenuhi
+
+// Selama lampu merah, kendaraan berhenti. jika tidak lagi merah, tidak berhenti
+
+// Selama masih hujan, Akan menggunakan payung, jika tidak lagi hujan, simpan payung
+
+// console.log(1);
+// console.log(2);
+// console.log(3);
+// console.log(4);
+// console.log(5);
+// console.log(6);
+// console.log(7);
+// console.log(8);
+// console.log(9);
+// console.log(10);
+
+// WHILE
+var angka = 99
+
+// Selama condition bernilai true
+while(angka <= 3){
+    console.log(angka);
+    angka++
+    // setelah baris terakhir di proses
+    // akan check condition pada while
+}
+
+console.log('Selesai WHILE');
+
+//  DO WHILE
+var nilai = 99
+
+do{
+    console.log(nilai);
+    nilai++
+}while(nilai <= 3)
+
+console.log('Selesai DO WHILE');
