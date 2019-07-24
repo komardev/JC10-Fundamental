e1fe96e4dcd0c83d87c79f17e69f85bd884ae317
rochafialvin <rochafi.dev@gmail.com>
2019-07-24 15:59:03 +0700

    Pasar

---
+++ tugas_pasar.js
@@ -0,0 +1,24 @@
+/*
+1. Memunculkan daftar Menu
+    - Ayam @ 25.000
+    - Ikan @ 50.000
+    - Sapi @ 75.000
+
+    - Minta input menu pilihan
+
+2. Minta inputan quantity 
+
+3. Tanyakan apakah ingin belanja lagi?
+    - mengggunakan 'confirm'
+
+    - Jika dia ingin belanja lagi, balik ke langkah 1
+    - Jika tidak ingin belanja lagi, lanjut ke langkah 4
+
+4. Munculkan Rincian belanja seperti screenshot
+    dan meminta inputan jumlah uang
+
+    - Jika uang yang dimasukkan kurang, ulangi langkah 4
+    - Jika tidak kurang, lanjut ke langkah terakhir
+
+Terakhir. Munculkan Rincian belanja, uang yg di bayar, berikut kembalian seperti screenshot
+*/
