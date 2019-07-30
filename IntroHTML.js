3224059cd0736b621e2d46f03e2ae780d85de208
rochafialvin <rochafi.dev@gmail.com>
2019-07-30 16:12:26 +0700

    selasa

---
+++ IntroHTML.js
@@ -0,0 +1,10 @@
+
+let myfunction = () => {
+    // mengambil element html yang memiliki id = 'nama
+    // something akan berupa object yg memiliki banyak property
+    // property value menyimpan data yang kita ketik
+    let something = document.getElementById('nama')
+
+    console.log(something.value);
+
+}
