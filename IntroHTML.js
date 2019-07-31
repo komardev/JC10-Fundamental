940427111777d04e2c324e4b1c63bb9c2038baea
rochafialvin <rochafi.dev@gmail.com>
2019-07-31 14:03:04 +0700

    asasasas

--- IntroHTML.js
+++ IntroHTML.js
@@ -1,10 +1,18 @@
 
 let myfunction = () => {
     // mengambil element html yang memiliki id = 'nama
-    // something akan berupa object yg memiliki banyak property
-    // property value menyimpan data yang kita ketik
-    let something = document.getElementById('nama')
+    // 'name' akan berupa object yg memiliki banyak property
+    // property 'value' menyimpan data yang kita ketik
+    let nama = document.getElementById('nama')
+    let usia = document.getElementById('usia')
+    // get document dengan query CSS
+    // get element input yg memiliki attribute name = gender
+    // dan property checked bernilai true
+    let gender = document.querySelector('input[name=gender]:checked')
+    let job = document.getElementById('pekerjaan')
 
-    console.log(something.value);
+    
+    let p = document.getElementById('disini')
+    p.innerHTML = `Nama: ${nama.value}, Usia: ${usia.value}, Gender: ${gender.value}, Job: ${job.value}`
 
 }
